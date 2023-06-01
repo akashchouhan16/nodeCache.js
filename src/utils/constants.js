d4125af96ad97d0a46a59b904395936b9b0b662f
Akash Chouhan <akash.c1500@gmail.com>
2023-06-01 14:43:08 +0530

    :wrench: init source for utils

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+/*******************************************
+ * copyright: nodeCache.js @github.com/akashchouhan16
+ * author: @akashchouhan16
+ * *****************************************
+*/
+
+module.exports = CONSTANTS = {
+    INVALID_MOD: `Invalid mode value. Only "none", "std", or "exp" are allowed`,
+    INVALID_TYPE: `Invalid type value. Only "default", "info", "warn", "error", or "fatal" are allowed`,
+    INVALID_PATH: `Invalid path value. Only "none", "console", or "file" are allowed`,
+    INVALID_INPUT: `Invalid inputs, nodeCache has been terminated`,
+    INIT: `nodeCache.js Initialized`,
+    INVALID_KEY_TYPE: `Invalid key provided. key must be of numeric or string type`,
+    INVALID_VALUE_TYPE: `Invalid value provided. Only number, string, or object type allowed`,
+    INVALID_TTL_TYPE: `Invalid ttl provided. ttl must be of numeric type in milliseconds(ms)`,
+    TERMINATE_MODE_EXP: `Process terminated due to fatal error. NodeCache mode is set to "exp"`
+}
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,16 +1,74 @@
-/**
+/*******************************************
+ * copyright: nodeCache.js @github.com/akashchouhan16
  * author: @akashchouhan16
- * copyright: 2023 nodeCache @github.com/akashchouhan16
- * */
+ * *****************************************
+*/
 
+const { log, warn, error } = require("console")
+const CONSTANTS = require("./constants")
+const validator = require("./validator")
 class Logger {
     constructor(options = {}) {
-        this.mode = options.mode || "std" // allowed: [std, exp]
-        this.type = options.type || "info" // allowed: [info, warn, error, fatal]
+
+        this.formatOptions = {
+            dateStyle: "short",
+            timeStyle: "short",
+            hour12: true,
+        };
+
+        if (validator(options)) {
+            this.mode = options.mode || "none" // permitted: [node, std, exp]
+            this.type = options.type || "info" // permitted: [info, warn, error, fatal]
+            this.path = options.path || "none" //permitted: [none, console, file]
+        } else {
+            const date = new Date().toLocaleString("en-US", this.formatOptions)
+            error(`[🍁 Err] ${date}: ${CONSTANTS.INVALID_INPUT}`)
+            process.exit(1);
+        }
     }
 
     log(message, options = {}) {
-        //TODO
+        if (!message || this.mode === "none")
+            return;
+        if (typeof message !== "string") {
+            if (typeof message === "object") {
+                message = JSON.stringify(message)
+            } else {
+                message = String(message)
+            }
+        }
+        const Options = {
+            mode: this.mode,
+            type: this.type,
+            path: this.path,
+            ...options
+        };
+
+
+        const date = new Date().toLocaleString("en-US", this.formatOptions);
+
+        switch (Options.type) {
+            case "default":
+            case "info":
+                log(`[🍁 Info] ${date}: ${message}`);
+                break;
+            case "warn":
+                warn(`[🍁 Warn] ${date}: ${message}`);
+                break;
+            case "error":
+                error(`[🍁 Error] ${date}: ${message}`);
+                break;
+            case "fatal":
+                error(`[🍁 Fatal] ${date}: ${message}`);
+                if (Options.mode === "exp") {
+                    error(`[🍁 Fatal] ${date}: ${CONSTANTS.TERMINATE_MODE_EXP}`)
+                    process.exit(1);
+                }
+                break;
+            default:
+                log(`[🍁 ${Options.type}] ${date}: ${message}`)
+                break;
+        }
     }
 }
 
