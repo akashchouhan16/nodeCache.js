3480f42304b3fbd321925d38d63787d3416f5040
Akash Chouhan <akash.c1500@gmail.com>
2023-07-17 15:39:15 +0530

    :maple_leaf: setup for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest/globals": true
+    },
+    plugins: ["jest"],
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "ignorePatterns": ["/dist/", "/tests/", "/*.mjs"],
+    "rules": {
+        quotes: ["error", "double", { "allowTemplateLiterals": true }]
+    }
+}
